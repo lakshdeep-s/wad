653549c896b2f527f156c0a5f4c52f4070b70499
Lakshdeep Singh <lakshdeep.code@gmail.com>
2024-12-13 10:46:12 +0530

    added Ride tracking for admin

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,3 +1,7 @@
+document.querySelector(".logout-btn").addEventListener("click", ()=> {
+    window.localStorage.removeItem("user")
+    window.location.href= "/login" 
+})
 document.getElementById("loginForm").addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent form submission from refreshing the page
 
@@ -7,12 +11,18 @@ document.getElementById("loginForm").addEventListener("submit", function (event)
     // Retrieve user credentials from localStorage
     const storedUser = JSON.parse(localStorage.getItem("user"));
 
-    if (storedUser && storedUser.username === username && storedUser.password === password) {
-        // Successful login
+    if (!storedUser) {
+        alert("User Credentials Not Found ! Create an account instead")
+    }
+
+    if (storedUser.isAdmin && storedUser.username ===username && storedUser.password === password) {
+        alert("Login Successfull")
+        window.location.href="/admin-dashboard"
+    } else if (storedUser.username === username && storedUser.password === password){
         alert("Login successful!");
         window.location.href = "/dashboard";
-    } else {
-        // Invalid credentials
-        alert("Invalid username or password. Please try again.");
+    }
+    else {
+        alert("Invalid username or password. Please try again.")
     }
 });
